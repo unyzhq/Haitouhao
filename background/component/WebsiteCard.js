477ee2ca774a3de725751fcc150e1a1babfae472
vay <3484466304@qq.com>
2023-11-01 13:16:03 +0800

    搜索栏绘制完成

--- background/component/WebsiteCard.js
+++ background/component/WebsiteCard.js
@@ -22,3 +22,4 @@ class WebsiteCard extends HTMLDivElement{
     }
     
 }
+export {WebsiteCard}
--- background/js/background.js
+++ background/js/background.js
@@ -1,28 +1,19 @@
-function createRemoteClick(fn,type,callback){
-    return function(){
-        try{
-            chrome.tabs.query({},function(tabs){
-                for(let tab of tabs){
-                    chrome.tabs.sendMessage(tab.id,type,callback)
-                }
-            })
-            fn()
-        }catch(err){
-            console.log(err)
-        }
-    }
+import {WebsiteCard} from "component/WebsiteCard"
+window.onload = ()=>{
+    
 }
 
-// window.onload = ()=>{
-//     let button = document.createElement("button")
-//     button.appendChild(document.createTextNode("click times: "))
-//     button.style.setProperty("width","100%")
-//     button.style.setProperty("height","30px")
-//     button.style.setProperty("background-color","aqua")
-//     button.addEventListener("click",createRemoteClick(()=>{},{select:true,text:"Web前端"},function(t){
-//         console.log(t)
-//         let text = button.firstChild
-//         button.innerText = text.substringData(0,12) + (Number(text.substringData(12,text.length)) + 1)
-//     }))
-//     document.body.appendChild(button)
+// function createRemoteClick(fn,type,callback){
+//     return function(){
+//         try{
+//             chrome.tabs.query({},function(tabs){
+//                 for(let tab of tabs){
+//                     chrome.tabs.sendMessage(tab.id,type,callback)
+//                 }
+//             })
+//             fn()
+//         }catch(err){
+//             console.log(err)
+//         }
+//     }
 // }
